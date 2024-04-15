b53533e15359bd2f463d681d3de6a76544be919d
Tas <zeynep.tas@onestic.com>
2024-04-15 22:03:49 +0300

    event serializer rpg_status field is updated

--- api/v1/serializers.py
+++ api/v1/serializers.py
@@ -10,7 +10,7 @@ class EventSerializer(ModelSerializer):
     """
     hotel_id = serializers.IntegerField()
     timestamp = serializers.DateTimeField(source="event_timestamp", read_only=True)
-    rpg_status = serializers.IntegerField(source="status")
+    rpg_status = serializers.ChoiceField(choices=[1, 2], source="status")
     room_id = serializers.UUIDField(source="room_reservation_id")
     night_of_stay = serializers.DateField(format="%Y-%m-%d")
 
